949947d4929df234a8c7be36b0fd80de1486eb73
QuangHuy <huy211020055@gmail.com>
2025-06-30 15:11:47 +0700

    Update

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+
+# Read data
+file_path = 'Datasets/diabetes.csv'
+df = pd.read_csv(file_path)
+
+# Data visualization and statistics
+# profile = ProfileReport(df, title="Diabetes report", explorative=True)
+# profile.to_file("Diabetes.html")
+
+# Data split
+target = "Outcome"
+x = df.drop([target], axis = 1)
+y = df[target]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# Data processing
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+# Train model
+model = SVC()
+model.fit(x_train, y_train)
+
+y_prd = model.predict(x_test)
+print(classification_report(y_test, y_prd))

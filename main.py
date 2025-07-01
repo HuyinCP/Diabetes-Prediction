2ce20c944da41c4675ebe37116c7a56cd7f492a0
QuangHuy <huy211020055@gmail.com>
2025-07-01 09:03:36 +0700

    Update

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pandas as pd
-from ydata_profiling import ProfileReport
+from fontTools.unicodedata import script
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from  sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
+from lazypredict.Supervised import LazyClassifier
 
 # Read data
 file_path = 'Datasets/diabetes.csv'
@@ -25,7 +29,26 @@ x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
 # Train model
-model = SVC()
-model.fit(x_train, y_train)
-y_prd = model.predict(x_test)
-print(classification_report(y_test, y_prd))
+# model = RandomForestClassifier(n_estimators=200, random_state=100, criterion="entropy")
+# model.fit(x_train, y_train)
+# y_prd = model.predict(x_test)
+# print(classification_report(y_test, y_prd))
+
+# params = {
+#     "n_estimators" : [100, 200, 300, 400],
+#     "criterion" : ["gini", "entropy", "log_loss"]
+# }
+# gird_search = GridSearchCV(estimator=RandomForestClassifier(random_state=42), param_grid=params, cv=5, verbose=2, scoring="recall")
+# gird_search.fit(x_train, y_train)
+#
+# print(gird_search.best_estimator_)
+# print(gird_search.best_score_)
+# print(gird_search.best_params_)
+#
+# y_prd = gird_search.predict(x_test)
+# print(classification_report(y_test, y_prd))
+# print((0.79 + 0.64) / 2)
+
+clf = LazyClassifier(verbose=0, ignore_warnings=True, custom_metric=None)
+models, predictions = clf.fit(x_train, x_test, y_train, y_test)
+print(models)
